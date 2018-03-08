965361f8aa51d745785bc2df8c6606ea1caeba7d
TheCompSciNoob <kyroschow@gmail.com>
2018-03-08 13:45:28 -0800

    user and player model classes created

--- app/src/main/java/com/example/chichow25/basementsandandroids/gamedata/Player.kt
+++ app/src/main/java/com/example/chichow25/basementsandandroids/gamedata/Player.kt
@@ -3,5 +3,11 @@ package com.example.chichow25.basementsandandroids.gamedata
 /**
  * Created by chichow25 on 3/8/18.
  */
-class Player {
+class Player(var aClass : String,
+             var race : String,
+             var speed : Int,
+             var level : Int,
+             var armor : Int,
+             var gold : Int,
+             var xp : Int) {
 }
---
+++ app/src/main/java/com/example/chichow25/basementsandandroids/gamedata/UserAccount.kt
@@ -0,0 +1,9 @@
+package com.example.chichow25.basementsandandroids.gamedata
+
+/**
+ * Created by chichow25 on 3/8/18.
+ */
+data class UserAccount(var email : String,
+                       var username : String,
+                       var password : String) {
+}
