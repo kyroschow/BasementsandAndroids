ab52067caa8d69b55e8ca248c8fc777a90a6c5fc
TheCompSciNoob <kyroschow@gmail.com>
2018-03-19 13:01:21 -0700

    retrofit responses done

---
+++ app/src/main/java/com/example/chichow25/basementsandandroids/retrofit/Equipment.kt
@@ -0,0 +1,9 @@
+package com.example.chichow25.basementsandandroids.retrofit
+
+/**
+ * Created by chichow25 on 3/19/18.
+ */
+data class Equipment(
+        val url: String,
+        val name: String
+)
