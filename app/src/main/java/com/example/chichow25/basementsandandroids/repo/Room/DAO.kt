f3c11b9535f916e49ed13f408d2b9d41a9db4363
Ryan <ryanliu393@gmail.com>
2018-03-29 13:43:57 -0700

    Chi's mum gay

---
+++ app/src/main/java/com/example/chichow25/basementsandandroids/repo/Room/DAO.kt
@@ -0,0 +1,19 @@
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy.REPLACE
+import android.arch.persistence.room.Query
+import com.example.chichow25.basementsandandroids.repo.Room.GameState
+
+@Dao
+interface WeatherDataDao {
+
+    @Query("SELECT * from gameState")
+    fun getAll(): List<GameState>
+
+    @Insert(onConflict = REPLACE)
+    fun insert(GameState: GameState)
+
+    @Query("DELETE from gameState")
+    fun deleteAll()
+}
