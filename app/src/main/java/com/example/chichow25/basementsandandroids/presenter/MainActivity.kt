5ca53c55f5c3f468b27b4d527cdb996c0729947a
TheCompSciNoob <kyroschow@gmail.com>
2018-04-11 13:06:17 -0700

    WiFi peer to peer receivers complete

--- app/src/main/java/com/example/chichow25/basementsandandroids/presenter/MainActivity.kt
+++ app/src/main/java/com/example/chichow25/basementsandandroids/presenter/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.chichow25.basementsandandroids.presenter
 
+import android.content.Context
+import android.content.IntentFilter
 import android.net.wifi.p2p.WifiP2pDeviceList
 import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
@@ -7,17 +9,24 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.WindowManager
 import com.example.chichow25.basementsandandroids.R
+import com.example.chichow25.basementsandandroids.repo.network.GameReceiver
 import io.realm.Realm
 
 class MainActivity : AppCompatActivity(), SplashScreenFragment.EventHandler, WifiP2pManager.PeerListListener, MainActivityView {
 
     private val TAG = "MainActivity"
+    //network
+    private lateinit var receiver: GameReceiver
+    private val intentFilter = IntentFilter().apply {
+        addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Initialize realm
-        Realm.init(this)
         //settings don't touch, for full screen
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
@@ -25,6 +34,25 @@ class MainActivity : AppCompatActivity(), SplashScreenFragment.EventHandler, Wif
         if (savedInstanceState == null)
             supportFragmentManager.beginTransaction().replace(R.id.mainActivityContainer, SplashScreenFragment()).commit()
         val presenter = MainActivityPresenter(this)
+
+        //network
+        val manager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+        val channel = manager.initialize(this, mainLooper, null)
+        receiver = GameReceiver(manager, channel, this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        //network
+        registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        //network
+        unregisterReceiver(receiver)
     }
 
     override fun joinPlayer(v: View) {
