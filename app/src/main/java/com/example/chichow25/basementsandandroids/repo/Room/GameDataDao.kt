aeefd54ba408d68a2da23250c156e957a567d464
TheCompSciNoob <kyroschow@gmail.com>
2018-04-13 13:13:29 -0700

    GameStateViewModel completed

--- app/src/main/java/com/example/chichow25/basementsandandroids/repo/Room/GameDataDao.kt
+++ app/src/main/java/com/example/chichow25/basementsandandroids/repo/Room/GameDataDao.kt
@@ -1,11 +1,8 @@
 package com.example.chichow25.basementsandandroids.repo.Room
 
 
-import android.arch.persistence.room.Dao
-import android.arch.persistence.room.Insert
+import android.arch.persistence.room.*
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
-import android.arch.persistence.room.Query
-import io.reactivex.Flowable
 
 /**
  * Created by Ryan Liu on 3/29/18.
@@ -15,11 +12,17 @@ import io.reactivex.Flowable
 interface GameDataDao {
 
     @Query("SELECT * from gameState")
-    fun getAll(): Flowable<List<GameState>>
+    fun getAll(): List<GameState>
 
     @Insert(onConflict = REPLACE)
-    fun insert(GameState: GameState)
+    fun insert(vararg gameStates: GameState)
+
+    @Delete
+    fun delete(vararg gameStates: GameState)
 
     @Query("DELETE from gameState")
     fun deleteAll()
+
+    @Update
+    fun update(gameState: GameState)
 }
--- app/src/main/java/com/example/chichow25/basementsandandroids/viewmodel/GameStateViewModel.kt
+++ app/src/main/java/com/example/chichow25/basementsandandroids/viewmodel/GameStateViewModel.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import com.example.chichow25.basementsandandroids.repo.Room.GameDataBase
+import com.example.chichow25.basementsandandroids.repo.Room.GameDataDao
 import com.example.chichow25.basementsandandroids.repo.Room.GameState
 
 /**
@@ -13,8 +14,16 @@ import com.example.chichow25.basementsandandroids.repo.Room.GameState
 class GameStateViewModel(app: Application) : AndroidViewModel(app) {
 
     //Room database
-    private val gameDataBase: GameDataBase = GameDataBase.getInstance(app)
+    private val gameDataDao: GameDataDao = GameDataBase.getInstance(app).gameDataDao()
     //LiveData
     val gameStatesLiveData: LiveData<List<GameState>>
-            = MutableLiveData<List<GameState>>().apply { value = gameDataBase.gameDataDao().getAll() }
+            = MutableLiveData<List<GameState>>().apply { value = gameDataDao.getAll() }
+
+    fun saveGameStateToDataBase(vararg gameStates: GameState) = gameDataDao.insert(*gameStates)
+
+    fun deleteGameStateFromDataBase(vararg gameStates : GameState) = gameDataDao.delete(*gameStates)
+
+    fun deleteAllGameStates() = gameDataDao.deleteAll()
+
+    fun updateGameState(gameState: GameState) = gameDataDao.update(gameState)
 }
