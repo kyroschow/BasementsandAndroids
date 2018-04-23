dd02aeaf2d8c8fe52d7f82039b3c579a08dbaef4
Ryan <ryanliu393@gmail.com>
2018-04-23 13:28:04 -0700

    Kotlin users hate dogs - Angela Zhang 2018

---
+++ app/src/main/java/com/example/chichow25/basementsandandroids/presenter/HostScreenFragment.kt
@@ -0,0 +1,18 @@
+package com.example.chichow25.basementsandandroids.presenter
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.chichow25.basementsandandroids.databinding.ActivityMainSplashBinding
+
+/**
+ * Created by chichow25 on 3/27/18.
+ */
+class HostScreenFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        
+    }
+
+}
