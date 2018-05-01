5d478e2d23ae09f2d4b6d10bf1e712b0e0db341b
Alexander Berry <berryalex123@gmail.com>
2018-05-01 15:15:34 -0700

    Database conversion implementation started

--- app/src/main/java/com/example/chichow25/basementsandandroids/repo/room/GameDataDao.kt
+++ app/src/main/java/com/example/chichow25/basementsandandroids/repo/room/GameDataDao.kt
@@ -2,31 +2,63 @@ package com.example.chichow25.basementsandandroids.repo.room
 
 
 import android.arch.lifecycle.LiveData
-import android.arch.persistence.room.*
-import android.arch.persistence.room.OnConflictStrategy.REPLACE
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
+import android.arch.persistence.room.Query
+import android.arch.persistence.room.Update
+import android.content.Intent
 import android.database.Cursor
+import com.example.chichow25.basementsandandroids.presenter.MainActivity
 
 /**
  * Created by Ryan Liu on 3/29/18.
  */
 
+//@Dao
+//interface GameDataDao {
+//
+//    @Query("SELECT * from gameState")
+//    fun getAll(): LiveData<List<GameState>>
+//    @Query("SELECT * from gameState")
+//    fun getCursor(): Cursor
+//
+//    @Insert(onConflict = REPLACE)
+//    fun insert(vararg gameStates: GameState)
+//
+//    @Delete
+//    fun delete(vararg gameStates: GameState)
+//
+//    @Query("DELETE from gameState")
+//    fun deleteAll()
+//
+//    @Update
+//    fun update(gameState: GameState)
+//}
+
 @Dao
-interface GameDataDao {
+abstract class GameDataDao {
+    @Query("SELECT * from gameState")
+    abstract fun getAll(): LiveData<List<GameState>>
 
     @Query("SELECT * from gameState")
-    fun getAll(): LiveData<List<GameState>>
-    @Query("SELECT * from gameState")
-    fun getCursor(): Cursor
-    
-    @Insert(onConflict = REPLACE)
-    fun insert(vararg gameStates: GameState)
+    abstract fun getCursor(): Cursor
+
+    fun insert(gameState: GameState){
+        val sqlRequest = "INSERT INTO gameState(playerPositionX, playerPositionY, enemyPositionX, enemyPositionY, playerHealth, enemyHealth, initiative, map, pieces) " +
+                "SELECT ?, ?, ?, ?, ?, ?, ?, ?, ? " +
+                "WHERE NOT EXISTS(SELECT 1 FROM (SELECT * FROM gameState ORDER BY id DESC LIMIT 1) WHERE playerPositionX = ?);"
+        val database = DatabaseStorage.gameDatabase.openHelper.writableDatabase
+        database.execSQL(sqlRequest, arrayOf(gameState.playerPositionX, gameState.playerPositionY, gameState.enemyPositionX, gameState.enemyPositionY,
+                gameState.playerHealth, gameState.enemyHealth, gameState.initiative, gameState.map, gameState.pieces))
+        MainActivity().applicationContext.sendBroadcast(Intent(DatabaseChangedReceiver.ACTION_DATABASE_CHANGED))
+    }
 
     @Delete
-    fun delete(vararg gameStates: GameState)
+    abstract fun delete(vararg gameStates: GameState)
 
     @Query("DELETE from gameState")
-    fun deleteAll()
+    abstract fun deleteAll()
 
     @Update
-    fun update(gameState: GameState)
+    abstract fun update(gameState: GameState)
 }
