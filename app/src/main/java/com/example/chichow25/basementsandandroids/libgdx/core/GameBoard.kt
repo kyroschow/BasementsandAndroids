e6857ca1c9ad122dc48f808163272b1c9b54c220
Alexander Berry <berryalex123@gmail.com>
2018-05-07 14:01:42 -0700

    Added libgdx

---
+++ app/src/main/java/com/example/chichow25/basementsandandroids/libgdx/core/GameBoard.kt
@@ -0,0 +1,11 @@
+package com.example.chichow25.basementsandandroids.libgdx.core
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/**
+ * Created by per6 on 5/7/18.
+ */
+class GameBoard: ApplicationAdapter(){
+    override fun create() {
+    }
+}
