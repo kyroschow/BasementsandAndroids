7ab38f0b465cac7ad6880eca99f01a497ca1bbd2
TheCompSciNoob <kyroschow@gmail.com>
2018-05-11 13:45:01 -0700

    test commit

--- core/src/com/bna/game/BnaGame.kt
+++ core/src/com/bna/game/BnaGame.kt
@@ -18,9 +18,11 @@ class BnaGame : ApplicationAdapter() {
     override fun render() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch?.begin()
-        batch?.draw(img, 0f, 0f)
-        batch?.end()
+        batch?.apply {
+            begin()
+            draw(img, 0f, 0f)
+            end()
+        }
     }
 
     override fun dispose() {
