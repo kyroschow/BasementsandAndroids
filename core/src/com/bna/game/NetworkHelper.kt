91b9090a479e253d1775409ace6405f5df4f84ff
Ryan <ryanliu393@gmail.com>
2018-05-17 14:07:46 -0700

    added new broadcast events to index.js

--- core/src/com/bna/game/NetworkHelper.kt
+++ core/src/com/bna/game/NetworkHelper.kt
@@ -2,11 +2,12 @@ package com.bna.game
 
 import io.socket.client.IO
 import io.socket.client.Socket
-import io.socket.emitter.Emitter
 import org.json.JSONException
 import org.json.JSONObject
 
-class NetworkHelper(val url: String, val port: Short) {
+class NetworkHelper(val url: String, val port: Short, val isHost: Boolean) {
+
+    private lateinit var id: String
 
     private val socket: Socket by lazy {
         IO.socket("$url:$port")
@@ -26,44 +27,33 @@ class NetworkHelper(val url: String, val port: Short) {
 
     fun disconnect() {
         try {
-
             socket.disconnect()
         } catch (e: Exception) {
             gdxLog(e.message, e)
         }
     }
 
-    fun configSocketEvents() {
-        socket.apply {
-            on(Socket.EVENT_CONNECT) {
-                gdxLog("Connected!")
-            }
-            on("SocketID") {
-                val data = it[0] as JSONObject
-                try {
-                    val id = data.getString("id")
-                    gdxLog("My ID: $id")
-                } catch (e: JSONException) {
-                    gdxLog(e.message, e)
-                }
-            }
-            on("NewPlayer") {
-                val data = it[0] as JSONObject
-                try {
-                    val id = data.getString("id")
-                    gdxLog("New Player Connected: $id")
-                } catch (e: JSONException) {
-                    gdxLog(e.message, e)
-                }
+    fun configSocketEvents(): Unit = with(socket) {
+        on(Socket.EVENT_CONNECT) {
+            gdxLog("Connected!")
+        }
+        on("SocketID") {
+            val data = it[0] as JSONObject
+            try {
+                id = data.getString("id")
+                gdxLog("My ID: $id")
+            } catch (e: JSONException) {
+                gdxLog(e.message, e)
             }
         }
-    }
-
-    inline fun Socket.on(event: String, crossinline listener: Socket.(args: Array<Any?>) -> Unit) {
-        on(event, object : Emitter.Listener {
-            override fun call(vararg args: Any?) {
-                listener(arrayOf(args))
+        on("NewPlayerJoined") {
+            val data = it[0] as JSONObject
+            try {
+                val id = data.getString("id")
+                gdxLog("New Player Joined: $id")
+            } catch (e: JSONException) {
+                gdxLog(e.message, e)
             }
-        })
+        }
     }
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var tempPlayers = [];
+var players = [];
+var host;
 
 server.listen(8080, function() {
     console.log("Server is now running...");
@@ -9,7 +12,17 @@ server.listen(8080, function() {
 io.on('connection', function(socket) {
     console.log("Player connected!");
     socket.emit('SocketID', { id: socket.id })
+    socket.broadcast.emit('NewPlayerJoined', { id: socket.id });
     socket.on('disconnect', function() {
         console.log("Player disconnected");
     });
 });
+
+function host(id, gameState) {
+    this.id = id;
+    this.state = gameState;
+}
+
+function player (id) {
+    this.id = id;
+}
