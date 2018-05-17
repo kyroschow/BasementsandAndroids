893b5b8f9e0cd0c7c3180648060cef1eee1ab139
Ryan Liu <ryanliu8888@yahoo.com>
2018-05-16 20:07:15 -0700

    Did some UI stuff. I don't have the right import for some of the stuff

--- core/src/com/bna/game/BnaGame.kt
+++ core/src/com/bna/game/BnaGame.kt
@@ -1,15 +1,28 @@
 package com.bna.game
 
+import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import io.socket.client.IO
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.utils.viewport.ScreenViewport
+import com.badlogic.gdx.utils.viewport.Viewport
 import io.socket.client.Socket
 import ktx.app.KtxGame
+import ktx.assets.toInternalFile
 import ktx.async.enableKtxCoroutines
 import ktx.inject.Context
+import ktx.scene2d.Scene2DSkin
+import ktx.style.*
+/*import com.ownedoutcomes.view.Game
+import com.ownedoutcomes.view.Menu*/ //TODO: make this work
 
 class BnaGame : KtxGame<Screen>() {
     private var batch: SpriteBatch? = null
@@ -22,11 +35,45 @@ class BnaGame : KtxGame<Screen>() {
     override fun create() {
         enableKtxCoroutines(asynchronousExecutorConcurrencyLevel = 1)
         context.register {
-
+            bindSingleton(TextureAtlas("skin.atlas"))
+            bindSingleton<Batch>(SpriteBatch())
+            bindSingleton<Viewport>(ScreenViewport())
+            bindSingleton(Stage(inject(), inject()))
+            bindSingleton(createSkin(inject()))
+            Scene2DSkin.defaultSkin = inject()
+            bindSingleton(this@BnaGame)
+            /*bindSingleton(Menu(inject(), inject()))   //TODO: incorrect imports
+            bindSingleton(Game(inject(), inject()))*/
         }
         batch = SpriteBatch()
         img = Texture("badlogic.jpg")
     }
+
+    fun createSkin(atlas: TextureAtlas): Skin = skin(atlas) { skin ->
+        add(defaultStyle, BitmapFont())
+        add("barbarian", FreeTypeFontGenerator("barbarian.ttf".toInternalFile())
+                .generateFont(FreeTypeFontGenerator.FreeTypeFontParameter().apply {
+                    /*borderWidth = 2f
+                    borderColor = Color.GRAY
+                    size = 50*/ //TODO:we can decide this later
+                }))
+        label {
+            font = skin[defaultStyle]
+        }
+        label("barbarian") {
+            font = skin["barbarian"]
+        }
+        textButton("barbarian") {
+            /*font = skin["barbarian"]
+            overFontColor = Color.GRAY
+            downFontColor = Color.DARK_GRAY*/ //TODO:decide later
+        }
+        window {
+            /*titleFont = skin[defaultStyle]
+            stageBackground = skin["black-alpha"]*/ //TODO:decide later
+        }
+    }
+
 
     override fun render() {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
