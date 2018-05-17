44fc1053f8bdb456b6804a5eeee9752ed077516a
TheCompSciNoob <kyroschow@gmail.com>
2018-05-17 12:30:05 -0700

    sdk fixed

--- android/src/com/bna/game/AndroidLauncher.kt
+++ android/src/com/bna/game/AndroidLauncher.kt
@@ -11,5 +11,6 @@ class AndroidLauncher : AndroidApplication() {
         super.onCreate(savedInstanceState)
         val config = AndroidApplicationConfiguration()
         initialize(BnaGame(), config)
+        //?
     }
 }
