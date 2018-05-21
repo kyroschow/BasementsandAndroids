a6acec4a74c2be3567acbff5f2d34825d4a0bd85
Ryan <ryanliu393@gmail.com>
2018-05-21 12:26:46 -0700

    translated some Java to Kotlin

--- core/src/com/bna/game/DragAndDrop.kt
+++ core/src/com/bna/game/DragAndDrop.kt
@@ -1,9 +1,22 @@
 package com.bna.game
 
-/*
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.tests.utils.GdxTest;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
 
-class DragAndDrop : GdxTest{
-    Stage stage
-}*/
+
+//import com.badlogic.gdx.tests.utils.GdxTest;
+
+class DragAndDrop /*: GdxTest*/{
+
+    var stage: Stage? = null
+
+    fun create(){
+        stage = Stage()
+        Gdx.input.setInputProcessor(stage)
+
+        val skin : Skin = Skin()
+        //skin.add("", )
+
+    }
+}
